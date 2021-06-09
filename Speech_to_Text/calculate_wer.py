68a2a5423e0dc535299bc864cde88a2edbb24de9
Hiren6 <70125843+Hiren6@users.noreply.github.com>
2021-06-09 14:04:26 +0530

    Deciding STT library

---
+++ Speech_to_Text/calculate_wer.py
@@ -0,0 +1,19 @@
+import jiwer
+
+correct_text = ""
+with open('orignal-text.txt', 'r') as fin:
+    correct_text = fin.read()
+
+my_text = ""
+with open("my_speech_output.txt", "r") as fin1:
+    my_text = fin1.read()
+
+ground_truth = correct_text
+hypothesis = my_text
+
+error = jiwer.wer(ground_truth, hypothesis)
+
+print(ground_truth)
+print("-----------------------------------------------------------------------------------------------------------------------------------------------")
+print(hypothesis)
+print("WER rate is: {}".format(error))
